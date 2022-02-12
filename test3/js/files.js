b377e0b32956c63eabf25f7cb4f77797ce0e2dfb
kakira <kurokuro917@gmail.com>
2022-02-12 13:19:31 +0900

    add test3

---
+++ test3/js/files.js
@@ -0,0 +1,5 @@
+function loadFiles(dir){
+   if (WebSite.isNW) return;
+	dir.rel('res.json').obj({"images":[{"name":"$pat_base","url":"images/base.png","pwidth":32,"pheight":32},{"name":"$pat_sample","url":"images/Sample.png"},{"name":"$pat_neko","url":"images/neko.png","pwidth":32,"pheight":32},{"name":"$pat_mapchip","url":"images/mapchip.png","pwidth":32,"pheight":32},{"name":"$icon_thumbnail","pwidth":200,"pheight":200,"url":"images/icon_thumbnail.png"}],"sounds":[]});
+	dir.rel('options.json').obj({"compiler":{"namespace":"user","defaultSuperClass":"kernel.Actor","dependingProjects":[{"namespace":"kernel","url":"js/kernel.js"}]},"run":{"mainClass":"user.Main","bootClass":"kernel.Boot","globals":{"$defaultFPS":60,"$imageSmoothingDisabled":true,"$soundLoadAndDecode":false}},"plugins":{"jquery_ui":1},"kernelEditable":false,"language":"tonyu","version":1639557686041});
+}
---
+++ test3/src/js/concat.js
@@ -0,0 +1,29 @@
+Tonyu.klass.define({
+  fullName: 'user.Main',
+  shortName: 'Main',
+  namespace: 'user',
+  superclass: Tonyu.classes.kernel.Actor,
+  includes: [],
+  methods: function (__superClass) {
+    return {
+      main :function _trc_Main_main() {
+        "use strict";
+        var _this=this;
+        
+      },
+      fiber$main :function _trc_Main_f_main(_thread) {
+        "use strict";
+        var _this=this;
+        //var _arguments=Tonyu.A(arguments);
+        var __pc=0;
+        
+        
+        _thread.retVal=_this;return;
+      },
+      __dummy: false
+    };
+  },
+  decls: {"methods":{"main":{"nowait":false}},"fields":{}}
+});
+
+//# sourceMappingURL=concat.js.map
