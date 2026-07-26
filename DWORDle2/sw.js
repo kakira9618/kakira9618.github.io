b16c39f6c1508c7013a9d345eb0a8cfc7d8197a9
kakira <kurokuro917@gmail.com>
2026-07-27 03:21:46 +0900

    DWORDle2: 更新ハッシュを反映

--- DWORDle2/sw.js
+++ DWORDle2/sw.js
@@ -2,7 +2,7 @@
 // DWORDle 2 の Service Worker。全資産をインストール時に事前キャッシュし、
 // オフラインでも完全動作させる（キャッシュ優先 + ネットワークフォールバック）。
 // キャッシュ名はコミットのハッシュ入りで、デプロイのたびに新しいキャッシュへ入れ替わる。
-const CACHE_NAME = "dwordle2-f1f3dec";
+const CACHE_NAME = "dwordle2-dbf648a";
 const PRECACHE = [
   "./",
   "index.html",
